ffa3baed248ee219363b6488e06b37b1572da0b6
Aimanan <petar.bochukov@abv.bg>
2017-04-28 18:33:16 +0300

    added some lines in the system file

--- public/scripts/configs/systemjs-config.js
+++ public/scripts/configs/systemjs-config.js
@@ -8,11 +8,12 @@ SystemJS.config({
         'requester': './scripts/requester.js',
         'user-controller': './scripts/controllers/user-controller.js',
         'players-controller': '/scripts/controllers/players-controller.js',
+        'home-controller': '/scripts/controllers/home-controller.js',
         'template-loader': './scripts/template-loader.js',
         'user-data': './scripts/data-service/user-data.js',
         'players-data': './scripts/data-service/players-data.js',
         'validator': './scripts/validator.js',
         'utils': './scripts/utils.js',
-        'local-storer': './scripts/local-storer.js'
+        
     }
 });
