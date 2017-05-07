47b9ee7626d78131eb4e58d2486eafa91e58aa9f
Borislav Borisov <borislavborisov22@abv.bg>
2017-05-07 23:14:16 +0300

    Setup for unit testing

---
+++ public/tests/configs/systemjs-config.js
@@ -0,0 +1,29 @@
+SystemJS.config({
+    transpiler: 'plugin-babel',
+    map: {
+        'plugin-babel': '../scripts/node_modules/systemjs-plugin-babel/plugin-babel.js',
+        'systemjs-babel-build': '../scripts/node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
+        'main': '../scripts/main.js',
+        'router': '../scripts/router.js',
+        'requester': '../scripts/requester.js',
+        'user-controller': '../scripts/controllers/user-controller.js',
+        'players-controller': '../scripts/controllers/players-controller.js',
+        'album-controller': '../scripts/controllers/album-controller.js',
+        'home-controller': '../scripts/controllers/home-controller.js',
+        'template-loader': '../scripts/template-loader.js',
+        'user-data': '../scripts/data-service/user-data.js',
+        'players-data': '../scripts/data-service/players-data.js',
+        'squad-data': '../scripts/data-service/squad-data.js',
+        'validator': '../scripts/validator.js',
+        'utils': '../scripts/utils.js',
+        'local-storer': '../scripts/local-storer.js',
+        'squad-controller': '../scripts/controllers/squad-controller.js',
+        'trade-controller': '../scripts/controllers/trade-controller.js',
+        'notificator': '../scripts/notificator.js',
+        'daily-bonus-controller': '../scripts/controllers/daily-bonus-controller.js',
+        'sharing-configurator': '../scripts/configs/sharing-configurator.js',
+
+        // testing files
+        'user-data-tests': './test-files/user-data-tests.js'
+    }
+});
---
+++ public/tests/test-files/user-data-tests.js
@@ -0,0 +1,14 @@
+import { userData } from 'user-data';
+
+mocha.setup('bdd');
+const { expect } = chai;
+
+
+describe('userData Tests', () => {
+
+    describe('userLogin tests', () => {
+
+    });
+});
+
+mocha.run();
